3f1b5bc7e176e488a89ee08166d7d240427d0303
pradhumna17 <rahatkar_p@ch.iitr.ac.in.com>
2020-09-10 16:44:30 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -247,9 +247,12 @@ app.post("/",function (req,res) {  //passport-local-mongoose to register our use
 
 
 
+let port=process.env.PORT;
+if(port == null||port ==""){
+  port=3000;
+}
 
 
-
-app.listen(process.env.PORT||3000,function () {
-    console.log("Server is listening on port 3000");
+app.listen(port,function () {
+    console.log("Server has started successfully");
 })
